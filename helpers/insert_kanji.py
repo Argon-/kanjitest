c45e8f45958cbfe0578aca4379a9255b2014857e
Julian <MyFakeAcc.4@googlemail.com>
2013-09-20 16:49:35 +0200

    Slight name adjustment

--- helpers/insert_kanji.py
+++ helpers/insert_kanji.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 #
-# This file is part of kanji_test
+# This file is part of kanjitest
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
